b6ca62b23fab5bdad17365e76254a1b6befcfc5d
Imran Akhmedov <xgcgdcgcgch@gmail.com>
2021-06-02 22:46:19 +0300

    Added commands
    Added commands:
    BAN, UNBAN, MUTE, UNMUTE, RULES

---
+++ bot.py
@@ -0,0 +1,89 @@
+import telebot
+from config import *
+
+
+bot = telebot.TeleBot(token) # Bot init
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, 'Я работаю!')
+
+@bot.message_handler(commands=['ban'])
+def ban(message):
+    chat_id = message.chat.id
+    if message.reply_to_message:
+
+        from_user = message.reply_to_message.from_user.id
+        #bot.send_message(chat_id, from_user)
+        try:
+            bot.kick_chat_member(id, from_user)
+            bot.send_message(chat_id, 'Правосудие свершилось!\nПользователь заблокирован!')
+        except Exception as e:
+            bot.send_message(chat_id, 'Произошла ошибка! Возможно пользователь являеться администратором!')
+            bot.send_message(chat_id, e)
+    else:
+        bot.send_message(chat_id, 'Эта команда должна быть ответом на сообщение!')
+
+@bot.message_handler(commands=['unban'])
+def unban(message):
+    chat_id = message.chat.id
+    if message.reply_to_message:
+
+        from_user = message.reply_to_message.from_user.id
+        #bot.send_message(chat_id, from_user)
+        try:
+            bot.unban_chat_member(id, from_user)
+            bot.send_message(chat_id, 'Кто то ошибся!\nПользователь разблокирован!')
+        except Exception as e:
+            bot.send_message(chat_id, 'Произошла ошибка!    ')
+            bot.send_message(chat_id, e)
+    else:
+        bot.send_message(chat_id, 'Эта команда должна быть ответом на сообщение!')
+
+@bot.message_handler(commands=['mute'])
+def unmute(message):
+    chat_id = message.chat.id
+    if message.reply_to_message:
+
+        from_user = message.reply_to_message.from_user.id
+        #bot.send_message(chat_id, from_user)
+        try:
+            bot.restrict_chat_member(id, from_user, can_send_messages=False)
+            bot.send_message(chat_id, 'Свершилось правосудие!\nПользователю заблокирована возможность писать сообщения')
+        except Exception as e:
+            bot.send_message(chat_id, 'Произошла ошибка!   ')
+            bot.send_message(chat_id, e)
+    else:
+        bot.send_message(chat_id, 'Эта команда должна быть ответом на сообщение!')
+
+@bot.message_handler(commands=['rules'])
+def rules(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, rules_banner )
+
+@bot.message_handler(commands=['unmute'])
+def mute(message):
+    chat_id = message.chat.id
+    if message.reply_to_message:
+
+        from_user = message.reply_to_message.from_user.id
+        #bot.send_message(chat_id, from_user)
+        try:
+            bot.restrict_chat_member(id, from_user, can_send_messages=True)
+            bot.send_message(chat_id, 'Пользователю возвращена способность писать сообщения')
+        except Exception as e:
+            bot.send_message(chat_id, 'Произошла ошибка! ')
+            bot.send_message(chat_id, e)
+    else:
+        bot.send_message(chat_id, 'Эта команда должна быть ответом на сообщение!')
+
+
+
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    chat_id = message.chat.id
+    #bot.send_message(chat_id, message.text)
+
+bot.polling()
---
+++ keyboard.py
@@ -0,0 +1,2 @@
+import teleBot
+from telebot import types
