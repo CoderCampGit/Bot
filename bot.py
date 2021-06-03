ada3013c1636cd0f00645b8ef7179d61bd5f0e27
Imran Akhmedov <xgcgdcgcgch@gmail.com>
2021-06-03 10:35:50 +0300

    Add bad words!

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import telebot
 from config import *
-
+import random
 
 bot = telebot.TeleBot(token) # Bot init
 
@@ -97,10 +97,30 @@ def mute(message):
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
+    from_user = message.from_user.id
     chat_id = message.chat.id
-    if message.text in languages:
-        bot.send_message(chat_id, 'Изыди')
-    else:
-        pass
+
+    for i in range(0, len(other_lang)):
+        if other_lang[i] in message.text.lower():
+            bot.send_message(chat_id, 'Изыди')
+    for i in range(0, len(top_lang)):
+        if top_lang[i] in message.text.lower():
+            bot.send_message(chat_id, 'Ай какой молодец!')
+
+    for i in range(0, len(bad_words)):
+        if bad_words[i] in message.text.lower():
+            answer = random.randint(1,4)
+            if answer == 1:
+                bot.send_message(chat_id,'А ты шалун')
+            elif answer == 2:
+                bot.send_message(chat_id,'Кто как обзывается, тот так и называется!')
+            elif answer == 3:
+                bot.send_message(chat_id, 'Шя как достану ствол правосудия')
+            elif answer == 4:
+                bot.send_message(chat_id, 'Осуждаю!')
+    if 'пасть порву' in message.text.lower():
+        bot.send_message(chat_id, 'Пасть свою себе порви')
+
+
 
 bot.polling()
--- config.py
+++ config.py
@@ -2,4 +2,12 @@ token = '' #Token bota
 id = -1001410284658 # ID беседы
 admins = [799770718, 1678253731] #ID администраторов
 rules_banner = 'Их нету\n'
-languages = ['Python', 'Ruby', 'JavaScript', 'Java', 'PHP']
+other_lang = ["c#", "c++", "дельфи", " ява ", "java", "php", " пхп", "swift", " свифт", " go ",
+              "javascript", "kotlin", " котлин", "rust", " раст ", "basic", " бейсик", " паскаль",
+              "golang", "pascal", "delphi", "perl", " перл ", "1c", " делфи", " си "]
+
+top_lang = ['python']
+
+
+bad_words = ["жопа", "дурак", "мудак", "durak", "бля", "хуй",
+             "хуя", 'сука']  
